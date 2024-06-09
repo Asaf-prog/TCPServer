9bc0afd75bb9158b16b50c6dc7b2f765d0f8e82c
asaf <128684083+Asaf-prog@users.noreply.github.com>
2024-06-09 18:21:32 +0300

    start to work on tcp server

---
+++ Server.h
@@ -0,0 +1,28 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define _CRT_NONSTDC_NO_DEPRECATE
+
+#include <iostream>
+using namespace std;
+#pragma comment(lib, "Ws2_32.lib")
+#include <winsock2.h>
+#include <string.h>
+#include "HTTP.h"
+#include <fstream>
+
+
+
+void importLib();
+SOCKET initiateSocket();
+void bindSocket(SOCKET* m_socket);
+bool addSocket(SOCKET id, int what, struct SocketState* sockets, int* socketsCount);
+void createSets(fd_set* waitRecv, fd_set* waitSend, struct SocketState* sockets);
+void filterUpcomingEvents(int* nfd, fd_set* waitRecv, fd_set* waitSend);
+void handleEvents(int* nfd, fd_set* waitRecv, fd_set* waitSend, struct SocketState* sockets, int* socketsCount);
+void finishingUp(SOCKET connSocket);
+void acceptConnection(int index, struct SocketState* sockets, int* socketsCount);
+void receiveMessage(int index, struct SocketState* sockets, int* socketsCount);
+void sendMessage(int index, struct SocketState* sockets, int* socketsCount);
+void removeSocket(int index, struct SocketState* sockets, int* socketsCount);
+void GetFile(struct Request* req);
---
+++ computerNetworkEx3.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+#include "Server.h"
+
+int main()
+{
+    fd_set waitRecv, waitSend;
+    int nfd;
+    struct SocketState sockets[MAX_SOCKETS] = { 0 };
+    int socketsCount = 0;
+
+    importLib();
+
+    SOCKET listenSocket = initiateSocket();
+
+    bindSocket(&listenSocket);
+
+    if (SOCKET_ERROR == listen(listenSocket, 5)) 
+    {
+        cout << "Time Server: Error at listen(): " << WSAGetLastError() << endl;
+        closesocket(listenSocket);
+        WSACleanup();
+        return 0;
+    }
+
+    // Add the listen socket to the array of all the sockets
+    addSocket(listenSocket, LISTEN, sockets, &socketsCount);
+
+    while (true)
+    {
+        createSets(&waitRecv, &waitSend, sockets);
+
+       filterUpcomingEvents(&nfd, &waitRecv, &waitSend);
+
+       handleEvents(&nfd, &waitRecv, &waitSend, sockets, &socketsCount);
+    }
+
+    finishingUp(listenSocket);
+
+    return 0;
+}
+
